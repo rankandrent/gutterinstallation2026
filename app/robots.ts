5a351a01d2f80cd8ab92ba27141ade5a8ab7d5e6
rankandrent <102636183+rankandrent@users.noreply.github.com>
2026-01-15 21:50:01 +0500

    robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -4,27 +4,11 @@ export default function robots(): MetadataRoute.Robots {
     const baseUrl = 'https://usgutterinstallation.com'
 
     return {
-        rules: [
-            {
-                userAgent: 'Googlebot',
-                allow: '/',
-                disallow: ['/api/', '/private/', '/admin/'],
-                crawlDelay: 0.5, // Be respectful but allow faster crawling
-            },
-            {
-                userAgent: 'Bingbot',
-                allow: '/',
-                disallow: ['/api/', '/private/', '/admin/'],
-                crawlDelay: 1,
-            },
-            {
-                userAgent: '*',
-                allow: '/',
-                disallow: ['/api/', '/private/', '/admin/'],
-            },
-        ],
-        sitemap: [
-            `${baseUrl}/sitemap.xml`,
-        ],
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: '/api/',
+        },
+        sitemap: `${baseUrl}/sitemap.xml`,
     }
 }
